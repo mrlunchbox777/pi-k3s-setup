2fe10385247aa4a74333e92caebf8f7693c84d99
Andrew Shoell <mrlunchbox777@gmail.com>
2021-02-06 17:14:19 -0700

    switching to this - https://mywiki.wooledge.org/BashFAQ/035

--- runme.sh
+++ runme.sh
@@ -116,6 +116,41 @@ while getopts "hostname:username:password:" opts; do
       [?]) write_block "argument $opts unknown" ;;
    esac
 done
+while :; do
+  case $1 in
+    -h|-\?|--help)
+      show_help    # Display a usage synopsis.
+      exit
+      ;;
+   -f|--file)       # Takes an option argument; ensure it has been specified.
+      if [ "$2" ]; then
+        file=$2
+        shift
+      else
+        die 'ERROR: "--file" requires a non-empty option argument.'
+      fi
+      ;;
+    --file=?*)
+      file=${1#*=} # Delete everything up to "=" and assign the remainder.
+      ;;
+    --file=)         # Handle the case of an empty --file=
+      die 'ERROR: "--file" requires a non-empty option argument.'
+      ;;
+    -v|--verbose)
+      verbose=$((verbose + 1))  # Each -v adds 1 to verbosity.
+      ;;
+    --)              # End of all options.
+      shift
+      break
+      ;;
+    -?*)
+      printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+      ;;
+    *)               # Default case: No more options, so break out of the loop.
+      break
+  esac
+  shift
+done
 # * &&& Write out safe variables and verify go &&&&
 if [ ! -z "${id_rsa_pub_location}" ]
 then
