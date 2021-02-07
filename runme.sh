e76a67e0451c01158789a477be58cb46f9b71d0a
Andrew Shoell <mrlunchbox777@gmail.com>
2021-02-07 12:41:36 -0700

    cleaning up formatting and calls a bit

--- runme.sh
+++ runme.sh
@@ -231,24 +231,29 @@ validate_variables() {
 setup_pi() {
   if [ ! -z "${id_rsa_pub_location}" ]
   then
-      ssh-keygen -f /home/${admin_username}/.ssh/id_rsa -N "${admin_ssh_password}"
-      export id_rsa_pub_location="/home/${admin_username}/.ssh/id_rsa.pub"
+    ssh-keygen -f /home/${admin_username}/.ssh/id_rsa -N "${admin_ssh_password}"
+    id_rsa_pub_location="/home/${admin_username}/.ssh/id_rsa.pub"
   fi
   echo -e "${admin_ssh_password}" | ssh-add -p "${id_rsa_pub_location}"
-  scp ${id_rsa_pub_location}/id_rsa.pub ${admin_username}@${hostname}:/home/${username}/.ssh/authorized_keys
+  scp ${id_rsa_pub_location}/id_rsa.pub ${admin_username}@${hostname}:/tmp/id_rsa.pub
   
   ssh pi@{hostname} -praspberry " \
     echo -e raspberry | sudo -S useradd -m -G sudo ${username}; \
-    echo -e ${password} | passwd ${username}; \
+    echo -e \"${password}\" | passwd ${username}; \
     echo -e raspberry | sudo -S sed -i 's/#PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config; \
+    cat /tmp/id_rsa.pub >> /home/${username}/.ssh/authorized_keys; \
+    rm /tmp/id_rsa.pub; \
+    echo -e raspberry | sudo -S chown ${username}:$username /home/${username}/.ssh/authorized_keys; \
+    echo -e raspberry | sudo -S chmod 755 /home/${username}/.ssh/authorized_keys; \
+    echo -e raspberry | sudo -S service ssh restart; \
   "
   
   ssh ${username}@${hostname} -i "${id_rsa_pub_location}" " \
-    echo -e ${password} | sudo -S userdel -r pi; \
-    echo -n " cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory " >> /boot/cmdline.txt; \
-    echo -e ${password} | sudo -S apt-get update; \
-    echo -e ${password} | sudo -S apt-get upgrade -y; \
-    echo -e ${password} | sudo -S apt-get autoremove -y; \
+    echo -e \"${password}\" | sudo -S userdel -r pi; \
+    echo -n \" cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory \" >> /boot/cmdline.txt; \
+    echo -e \"${password}\" | sudo -S apt-get update; \
+    echo -e \"${password}\" | sudo -S apt-get upgrade -y; \
+    echo -e \"${password}\" | sudo -S apt-get autoremove -y; \
     sudo reboot now; \
   "
   
@@ -257,20 +262,18 @@ setup_pi() {
   
   if [ -z /usr/local/bin/k3sup ]
   then
-      write_block "Installing k3s"
-      curl -sLS https://get.k3sup.dev | sh
-      sudo install k3sup /usr/local/bin/
+    write_block "Installing k3s"
+    curl -sLS https://get.k3sup.dev | sh
+    sudo install k3sup /usr/local/bin/
   fi
   
   k3sup install --host ${hostname} --user ${username} --ssh-key ${id_rsa_pub_location} --cluster
   
   if [ ! -z "${cluster_server_name}" ]
   then
-      k3sup join --host ${hostname} --user ${username} --server-host ${cluster_server_name} --server-user ${username} --ssh-key ${id_rsa_pub_location} --server
+    k3sup join --host ${hostname} --user ${username} --server-host ${cluster_server_name} --server-user ${username} --ssh-key ${id_rsa_pub_location} --server
   fi
 }
-
-write_block "Starting Up"
 
 # Parse Parameters
 while :; do
@@ -408,6 +411,8 @@ while :; do
   esac
   shift
 done
+
+write_block "Starting Up"
 
 if [ $force_help -eq 1 ]
 then
